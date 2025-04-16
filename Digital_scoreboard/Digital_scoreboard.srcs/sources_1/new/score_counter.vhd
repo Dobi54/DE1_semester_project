ec41266a43563e50270f7fc00fb37e637e799c7f
Dobi54 <dobias.david.cz@seznam.cz>
2025-04-16 18:43:43 +0200

    Update score_counter.vhd

--- Digital_scoreboard/Digital_scoreboard.srcs/sources_1/new/score_counter.vhd
+++ Digital_scoreboard/Digital_scoreboard.srcs/sources_1/new/score_counter.vhd
@@ -5,8 +5,8 @@ use IEEE.NUMERIC_STD.ALL;
 entity score_counter is
     Port ( 
         clk              : in  STD_LOGIC;  -- hodinový signál
-        pointup_team1    : in  STD_LOGIC;  -- přidání bodu týmu 1
-        pointup_team2    : in  STD_LOGIC;  -- přidání bodu týmu 2
+        pointup_team1    : in  STD_LOGIC;  -- p?idání bodu týmu 1
+        pointup_team2    : in  STD_LOGIC;  -- p?idání bodu týmu 2
         rst              : in  STD_LOGIC;  -- reset skóre
         score_team1_unit : out STD_LOGIC_VECTOR(3 downto 0); -- jednotky tým 1
         score_team1_tens : out STD_LOGIC_VECTOR(3 downto 0); -- desítky tým 1
@@ -23,21 +23,23 @@ architecture Behavioral of score_counter is
     signal team2_unit : integer range 0 to 9 := 0;
     signal team2_tens : integer range 0 to 9 := 0;
 
+    signal last_pointup_team1 : std_logic := '0';
+    signal last_pointup_team2 : std_logic := '0';
+
 begin
 
     process(clk)
     begin
         if rising_edge(clk) then
             if rst = '1' then
-                -- reset skóre obou týmů
+                -- reset skóre obou tým?
                 team1_unit <= 0;
                 team1_tens <= 0;
                 team2_unit <= 0;
                 team2_tens <= 0;
-
             else
-                -- přidání bodu pro tým 1
-                if pointup_team1 = '1' then
+                -- náb?žná hrana stisku tla?ítka pro tým 1
+                if pointup_team1 = '1' and last_pointup_team1 = '0' then
                     if team1_unit = 9 then
                         team1_unit <= 0;
                         if team1_tens < 9 then
@@ -48,8 +50,8 @@ begin
                     end if;
                 end if;
 
-                -- přidání bodu pro tým 2
-                if pointup_team2 = '1' then
+                -- náb?žná hrana stisku tla?ítka pro tým 2
+                if pointup_team2 = '1' and last_pointup_team2 = '0' then
                     if team2_unit = 9 then
                         team2_unit <= 0;
                         if team2_tens < 9 then
@@ -59,12 +61,15 @@ begin
                         team2_unit <= team2_unit + 1;
                     end if;
                 end if;
-
             end if;
+
+            -- uložení p?edchozích stav? tla?ítek
+            last_pointup_team1 <= pointup_team1;
+            last_pointup_team2 <= pointup_team2;
         end if;
     end process;
 
-    -- převod výstupů na std_logic_vector
+    -- p?evod výstup? na std_logic_vector
     score_team1_unit <= std_logic_vector(to_unsigned(team1_unit, 4));
     score_team1_tens <= std_logic_vector(to_unsigned(team1_tens, 4));
     score_team2_unit <= std_logic_vector(to_unsigned(team2_unit, 4));
